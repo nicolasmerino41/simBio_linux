50424c65342c30f34469ef9c4f3908719933d52a
Nico Merino <nicolasmerino41@gmail.com>
2024-11-18 17:11:05 +0100

    Updated trials to check Sensitivity Analysis

--- trial.jl
+++ trial.jl
@@ -1,17 +1,12 @@
-
 using Distributions
 using Random
 
 # OpenMOLE-provided variables (assumed to be defined)
-# mu                 # Competition coefficient (0 to 1)
-# NPP                # Net Primary Productivity (10 to 10000)
-# num_predators      # Number of predator species
-# num_herbivores     # Number of herbivore species
-# H0_mean            # Mean characteristic density of herbivores
-# Optionally, you can define 'seed' for reproducibility
+# total_biomass
 
 # Set random seed for reproducibility (optional)
 # Random.seed!(seed)
+
 result = total_biomass * rand()
-# Output the result (OpenMOLE will capture the variable 'total_biomass')
-println("total_biomass = ", total_biomass, " result = ", result)
+# Output the result (OpenMOLE will capture the variable 'result')
+println("result = ", result)
